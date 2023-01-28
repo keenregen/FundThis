47daf6fef89bc63bb8a812dbc119099b8fd273a6
keenregen <keenregen@gmail.com>
2023-01-28 14:13:31 +0300

    comm 1.2

--- FundThis.sol
+++ FundThis.sol
@@ -4,15 +4,36 @@
 
 // SPDX-License-Identifier: Blank
 
+// import the interface
+import "./node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 pragma solidity ^0.8.8;
 
+// 1e18 = 1 * 10 * 10**18 wei = 1 ETH
+
 contract FundThis {
+
+    uint256 public minUsd = 1 * 1e18;
 
     function fundThis() public payable {
         // set min funding value with require keyword
         // if require condition is not met, the ops before are undone and gas remaining is sent back
         // msg.value : how much money is added to be sent
-        require(msg.value > 1e18, "min 1 ETH is needed"); // 1e18 = 1 * 10 * 10**18 wei = 1 ETH
+        require(getConverted(msg.value) >= minUsd, "min $1 is needed"); 
+    }
 
+    function getPrice() public view returns (uint256) {
+        // abi
+        // contract address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e  (eth / usd goerli testnet)
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        // ETH in usd (8 decimal places, should be 18 dec places as ETH and should be uint256 as ETH)
+        return uint256(price) * 1e10;
+    }   
+
+    function getConverted(uint256 ethAmount) public view returns (uint256){
+        // ETH in USD
+        uint256 ethAmountInUsd = (getPrice() * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 }
